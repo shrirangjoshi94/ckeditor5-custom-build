aafae298f0275671c5858162bba95153cdc0ba03
Shrirang joshi <sh.joshi@easternenterprise.com>
2020-05-03 20:34:56 +0530

    Initial import.

---
+++ src/ckeditor.js
@@ -0,0 +1,38 @@
+/**
+ * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+
+export default class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Autoformat,
+	BlockQuote,
+	Bold,
+	Heading,
+	Italic,
+	Link,
+	List,
+	PasteFromOffice,
+	Alignment,
+	FontSize,
+	FontFamily,
+	Essentials,
+	Paragraph
+];
+
